a8a23181e45197434a1d2fefbd903907f8d6202b
yagneshgondaliya <yagnesh.g03@gmail.com>
2022-08-25 21:39:44 -0700

    This is Coditional Operators by Bukky...

--- CoditionalOperators.java
+++ CoditionalOperators.java
@@ -1,2 +1,6 @@
-package PACKAGE_NAME;public class CoditionalOperators {
+class CoditionalOperators {
+    public static void main(String[] args) {
+        int age =21;
+        System.out.println(age >50 ? "You are old" : "You are Young");
+    }
 }
