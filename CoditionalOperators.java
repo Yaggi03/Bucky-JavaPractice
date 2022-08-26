ca0d4789a7403177210b1b1bb15f99011426ca03
yagneshgondaliya <yagnesh.g03@gmail.com>
2022-08-26 12:18:45 -0700

    test commit

--- CoditionalOperators.java
+++ CoditionalOperators.java
@@ -1,6 +1,6 @@
 class CoditionalOperators {
     public static void main(String[] args) {
         int age =21;
-        System.out.println(age >50 ? "You are old" : "You are Young");
+        System.out.println(age >50 ? "You are old" : "Hey Tuna meet is the lunch today!!");
     }
 }
