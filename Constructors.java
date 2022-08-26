d3353ce8953aead475f2cdeeeb675b396b2b5bd4
yagneshgondaliya <yagnesh.g03@gmail.com>
2022-08-25 21:09:44 -0700

    This is Constructors.....

--- Constructors.java
+++ Constructors.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Constructors {
+public class Constructors {
+    private String girlName;
+
+    public Constructors(String name) {
+        girlName=name;
+    }
+
+    public void setName (String name) {
+        girlName=name;
+    }
+    public String getName () {
+        return girlName;
+    }
+    public void saying() {
+        System.out.printf("Your first gf was %s\n", getName());
+    }
 }
--- Constructors2.java
+++ Constructors2.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class Constructors2 {
+class Constructors2 {
+    public static void main(String args[]) {
+        Constructors ConstructorsObject = new Constructors("Kelsey");
+        Constructors ConstructorsObject2  = new Constructors("Nicole");
+        ConstructorsObject.saying();
+        ConstructorsObject2.saying();
+    }
+
 }
