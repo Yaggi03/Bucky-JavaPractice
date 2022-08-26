602af6ef3f6884bfc09b5d439e63a5f796844bf8
yagneshgondaliya <yagnesh.g03@gmail.com>
2022-08-26 12:28:44 -0700

    This video is for DoWhileLoop by BuckkyBoston....

--- DoWhileLoop.java
+++ DoWhileLoop.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class DoWhileLoop {
+class DoWhileLoop {
+    public static void main(String[] args) {
+        int counter = 0;
+        do {
+            System.out.println(counter);
+            counter++;
+        }while(counter<=10);
+    }
 }
